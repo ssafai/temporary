78c64634a469e1a990882df36bc11717e8b9d468
Sasan Safai <safai@web.de>
2019-01-03 22:50:39 +0100

    initial template

---
+++ site/search.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="assets/tm/css/tm_docs.css" type="text/css" media="screen">
+    <script src="assets/assets/js/html5shiv.js"></script>    
+    <script src="assets/assets/js/respond.min.js"></script>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+</head>
+
+<body class="search">
+<!--==============================header=================================-->
+   <header id="header">
+        <div class="block-1 clearfix">
+            <div class="container">
+                <div class="top_menu clearfix">
+                    <h1 class="navbar-brand navbar-brand_ wow fadeInLeft" data-wow-delay="0.2s"><a href="index.html">Security<span>consultant</span></a></h1>
+                    <nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">
+                        <ul class="nav sf-menu clearfix">
+                            <li><a href="index.html">Home</a></li>
+                            <li class="sub-menu"><a href="index-1.html">About</a>
+                                <ul class="submenu">
+                                    <li><a href="#">History</a></li>
+                                    <li><a href="#">Clients</a>
+                                        <ul class="submenu">
+                                            <li><a href="#">Lorem</a></li>
+                                            <li><a href="#">Dolorsit</a></li>
+                                            <li><a href="#">Ametrew</a></li>
+                                        </ul> 
+                                    </li>
+                                    <li><a href="#">Consultation</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="index-2.html">Products</a></li>
+                            <li><a href="index-3.html">News & Events</a></li>
+                            <li><a href="index-4.html">Contacts</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+<section id="content">
+  <div class="block-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+         <header>
+           <div class="with-border">
+             <h2 class="h2-pad">Search result:</h2>
+           </div>
+         </header>
+          <div id="search-results"></div>
+      </div>
+    </div>
+  </div>
+</section>
+<footer id="footer" class="pattern-2">
+    <div class="container">
+        <p class="wow fadeInUp">Copyright &copy; <span id="copyright-year"></span>|<a href="index-5.html">Privacy Policy</a></p>
+    </div>
+</footer>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
